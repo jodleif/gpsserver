879200db3cfae25aabfdb39f4c45b7c3c4d93d04
Jo Øivind Gjernes <jodleif@gmail.com>
2015-11-28 16:29:11 +0100

    misc: Start server on port 1234

--- TcpServer/main.cpp
+++ TcpServer/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include "tcpserver.h"
 
 int main()
 {
 	std::cout << "Test driver / entry point for tcpserver\n";
-
+	gps::server::tcpserver server(1234);
+	server.listen();
 	return 0;
 }
